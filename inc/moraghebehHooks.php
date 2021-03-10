d9b4f5526fb765a2043db5c79292d6157efa5347
hamzeh <h.pourshabanan@live.com>
2021-03-10 21:12:32 +0330

    moraghebehHook update for construction page

--- inc/moraghebehHooks.php
+++ inc/moraghebehHooks.php
@@ -5,3 +5,14 @@
 add_action('after_some-page_wrapper', 'insertResults');
 
 add_filter( 'arbayiin_startDate', 'setStartDate');
+
+add_action('admin_init', 'redirectToUnderConstruction');
+add_action('wp', 'redirectToUnderConstruction');
+
+function redirectToUnderConstruction() {
+
+    if(get_current_user_id() !=1 && !is_page('construction')) {
+        // Not logged in, not the login page and not the dashboard
+        exit( wp_redirect( home_url( '/construction' ) ) );
+    }
+}
