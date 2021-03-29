b7189a8eda9dd5e2e8bb1c815314034593b3d076
hamzeh <h.pourshabanan@live.com>
2021-03-29 12:32:19 +0430

    correct tozihat section

--- template-parts/content-popup.php
+++ template-parts/content-popup.php
@@ -1,7 +1,9 @@
 <div style="display: none;" class="pop-outer">
     <div class="pop-inner">
+        <div class="pop-inner__head">
         <button class="close-popup">X</button>
         <hr/>
+        </div>
         <h5></h5>
         <p></p>
 
