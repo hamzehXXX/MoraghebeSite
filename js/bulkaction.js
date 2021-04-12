82c86de918c29f8d8970014e7757370aff4ade16
hamzeh <h.pourshabanan@live.com>
2021-04-13 02:35:01 +0430

    improve salek dastoor takhsis

--- js/bulkaction.js
+++ js/bulkaction.js
@@ -11,14 +11,16 @@ jQuery(document).ready(function() {
     });
 });
 
-
+var doneTheStuff = true;
 jQuery(document).ready(function() {
 
     // Watch the bulk actions dropdown, looking for custom bulk actions
     jQuery("#bulk-action-selector-top, #bulk-action-selector-bottom").on('change', function(e){
         var $this = jQuery(this);
-        if ( $this.val().includes('takhsisedarb') ) {
-            $this.after(jQuery("<input>", { type: 'text', placeholder: "تکرار", name: "de_bulk_takhsis_to_selected" }).addClass("de-custom-bulk-actions-selected"));
+        jQuery(".de-custom-bulk-actions-elements").remove();
+        if ( $this.val().includes('takhsisedarb') && doneTheStuff) {
+            $this.after(jQuery("<input>", { type: 'text', placeholder: "تکرار", name: "de_bulk_takhsis_to_selected"}).addClass("de-custom-bulk-actions-selected"));
+            doneTheStuff = false;
         } else {
             jQuery(".de-custom-bulk-actions-elements").remove();
         }
