5f955a62485e94567d688c316e3b92b6e2194b61
hamzeh <h.pourshabanan@live.com>
2021-04-13 11:39:49 +0430

    debug bulkaction.js for dastoor takhsised and takhsis_arb_bulk for dastoor notices

--- js/bulkaction.js
+++ js/bulkaction.js
@@ -1,28 +1,23 @@
-jQuery(document).ready(function() {
-    // Watch the bulk actions dropdown, looking for custom bulk actions
-    jQuery("#bulk-action-selector-top, #bulk-action-selector-bottom").on('change', function(e){
-        var $this = jQuery(this);
-
-        if ( $this.val() == 'takhsis_to_all' ) {
-            $this.after(jQuery("<input>", { type: 'text', placeholder: "تکرار", name: "de_bulk_takhsis_to_all" }).addClass("de-custom-bulk-actions-elements"));
-        } else {
-            jQuery(".de-custom-bulk-actions-elements").remove();
-        }
-    });
-});
-
 var doneTheStuff = true;
 jQuery(document).ready(function() {
 
     // Watch the bulk actions dropdown, looking for custom bulk actions
     jQuery("#bulk-action-selector-top, #bulk-action-selector-bottom").on('change', function(e){
         var $this = jQuery(this);
-        jQuery(".de-custom-bulk-actions-elements").remove();
-        if ( $this.val().includes('takhsisedarb') && doneTheStuff) {
-            $this.after(jQuery("<input>", { type: 'text', placeholder: "تکرار", name: "de_bulk_takhsis_to_selected"}).addClass("de-custom-bulk-actions-selected"));
+        console.log($this.val());
+
+        if ( $this.val() == 'takhsis_to_all' ) {
+            $this.after(jQuery("<input>", { type: 'text', placeholder: "تکرار", name: "de_bulk_takhsis_to_all" }).addClass("de-custom-bulk-actions-elements"));
+        } else if ($this.val().includes('takhsisedarb')){
+            if (doneTheStuff) {
+                $this.after(jQuery("<input>", { type: 'text', placeholder: "تکرار", name: "de_bulk_takhsis_to_selected"}).addClass("de-custom-bulk-actions-selected"));
+            }
             doneTheStuff = false;
-        } else {
+        }
+        else {
             jQuery(".de-custom-bulk-actions-elements").remove();
+            jQuery(".de-custom-bulk-actions-selected").remove();
+            doneTheStuff = true;
         }
     });
 });
