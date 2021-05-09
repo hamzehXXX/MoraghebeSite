b599d5537b0fb08066d4ee8db798036d5de5046a
hamzeh <h.pourshabanan@live.com>
2021-05-10 01:01:09 +0430

    show questions for all users

--- header.php
+++ header.php
@@ -38,11 +38,11 @@ if (!is_user_logged_in()) {
                             </li>
 
                         <?php }
-                       if (in_array('reporter', $ourCurrentUser->roles)){ ?>
+//                       if (in_array('reporter', $ourCurrentUser->roles)){ ?>
                            <li <?php if (get_post_type() == 'questioncat')echo 'class="current-menu-item"'; ?>>
                                <a style="color: red" href="<?php echo get_post_type_archive_link('questioncat'); ?>">پرسش و پاسخ</a>
                            </li>
-                       <?php } ?>
+<!--                       --><?php //} ?>
 
 					   <li <?php if (get_post_type() == 'arbayiin')echo 'class="current-menu-item"'; ?>><a href="<?php echo get_post_type_archive_link('arbayiin'); ?>">اربعینیات</a></li>
                         <li <?php if (get_post_type() == 'event' OR is_page('past-events')) echo 'class="current-menu-item"'?>><a href="<?php echo get_post_type_archive_link('event');?>">رویدادها</a></li>
